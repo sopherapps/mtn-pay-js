7e62ef0436bce4130492e30b5ce9a6289682be6a
Martin Ahindura <martinahindura@gmail.com>
2019-06-03 18:30:01 +0300

    Add the account product

--- src/utils/account.ts
+++ src/utils/account.ts
@@ -1,12 +1,20 @@
 /// https://ericssonbasicapi2.azure-api.net/collection/v1_0/account/balance
 
-import { IApiToken } from './sharedTypes';
+import { BaseProduct } from './sharedTypes';
+import getResources, { IResource } from './repository';
 
 export interface IAccountConfig {
   subscriptionKey: string,
   targetEnvironment?: string,
   apiuserId: string,
   apiKey: string,
+  authBaseURL?: string,
+  baseURL?: string,
+}
+
+export interface IAccountDetails {
+  balance: number;
+  currency: string;
 }
 
 
@@ -23,32 +31,46 @@ export interface IAccountConfig {
  * @public @method authenticate()
  * @private @method queryApi()
  */
-export default class Account {
+export default class Account extends BaseProduct {
 
-  public balance: number;
-  public currency: string;
-  public apiToken: IApiToken;
-  public apiKey: string;
-  public apiUserId: string;
-  public targetEnvironment: string;
+  public lastModified: Date | undefined;
+  private details: IAccountDetails | undefined;
+  private accountResource: IResource;
+
 
   constructor(config: IAccountConfig) {
-    this.targetEnvironment = config.targetEnvironment || 'sandbox';
+    const authBaseURL = (
+      config.authBaseURL || 'https://ericssonbasicapi2.azure-api.net/collection');
+    const baseURL = config.baseURL || 'https://ericssonbasicapi2.azure-api.net/collection/v1_0';
+    super({ ...config, authBaseURL });
+
+    const accountUrl = 'account';
+    this.accountResource = getResources(
+      [accountUrl], baseURL, this.commonHeaders)[accountUrl];
   }
 
-  public async getDetails() {
-    await this.queryApi();
+  public async getDetails(forceRefresh = false) {
+    await this.authenticate();
+    if (forceRefresh || !this.lastModified) {
+      const headers: any = {
+        'Authorization': `Bearer ${this.apiToken ? this.apiToken.accessToken : ''}`
+      };
+      try {
+        const response = await this.accountResource.getOne('balance', headers);
+        if (response.status === 200) {
+          this.details = {
+            balance: parseFloat(response.data.availableBalance),
+            currency: response.data.currency
+          };
+          this.lastModified = new Date();
+        } else {
+          throw new Error(response.data.message || 'Error making request')
+        }
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    }
+    return this.details;
   }
-
-  /**
-   * Gets an Oauth token to use to do queries
-   */
-  public authenticate() { }
-
-  /**
-   * Queries the account balance endpoint of API
-   * and updates this instance's properties
-   */
-  private async queryApi() { }
-
 }
--- src/utils/sharedTypes.ts
+++ src/utils/sharedTypes.ts
@@ -1,3 +1,5 @@
+import getResources, { IResource } from "./repository";
+
 /// Shared Types
 
 export interface IApiToken {
@@ -5,4 +7,66 @@ export interface IApiToken {
   tokenType: string,
   expiresIn: number,
   createdAt: Date,
+}
+
+export class BaseProduct {
+  public commonHeaders: {
+    'Content-Type': string,
+    'Ocp-Apim-Subscription-Key': string,
+    'Authorization': string,
+    'X-Target-Environment': string,
+  };
+  private authResource: IResource;
+  protected apiToken: IApiToken | undefined;
+
+  constructor({
+    authBaseURL,
+    authUrl = 'token/',
+    subscriptionKey,
+    apiuserId,
+    apiKey,
+    targetEnvironment = 'sandbox'
+  }: {
+    authBaseURL: string,
+    authUrl?: string,
+    subscriptionKey: string,
+    apiuserId: string,
+    apiKey: string,
+    targetEnvironment?: string
+  }) {
+    authBaseURL = (
+      authBaseURL || `https://ericssonbasicapi2.azure-api.net/collection`);
+    this.commonHeaders = {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': subscriptionKey,
+      'Authorization': `Basic ${btoa(`${apiuserId}:${apiKey}`)}`,
+      'X-Target-Environment': targetEnvironment,
+    };
+    this.authResource = getResources(
+      [authUrl], authBaseURL, this.commonHeaders)[authUrl];
+  }
+
+  protected async authenticate() {
+    if (this.shouldAuthenticate) {
+      const response = await this.authResource.create({});
+      if ([200, 201, 202].includes(response.status)) {
+        this.apiToken = {
+          accessToken: response.data.access_token,
+          tokenType: response.data.token_type,
+          expiresIn: response.data.expires_in,
+          createdAt: new Date(),
+        };
+      }
+    }
+  }
+
+  protected shouldAuthenticate() {
+    if (!this.apiToken) return true;
+    const tokenAgeInSeconds = this.secondsSince(this.apiToken.createdAt);
+    return tokenAgeInSeconds >= this.apiToken.expiresIn;
+  }
+
+  protected secondsSince(startingTime: Date) {
+    return (new Date().getTime() - startingTime.getTime()) / 1000;
+  }
 }
